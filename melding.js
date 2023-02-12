7948d7ff186586e4a8cee9fa59fe4f84ad4b41da
leavemygame <115145061+leavemygame@users.noreply.github.com>
2023-02-12 13:24:38 +0100

    qsdf
    qsdf

---
+++ melding.js
@@ -0,0 +1,7 @@
+<script>
+    function melding()
+    {
+        alert("als je terug naar de homepagina wilt, klik dan op ons logo linksbovenaan")
+    }    
+</script>
+// de script op meerdere paginas gebruiken wilt niet werken
