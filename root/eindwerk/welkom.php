d501e47abf96f0510d65872b050976b6bc6c635c
leavemygame <115145061+leavemygame@users.noreply.github.com>
2023-04-20 11:54:57 +0200

    qsdf
    qsdf

--- root/eindwerk/welkom.php
+++ root/eindwerk/welkom.php
@@ -3,9 +3,9 @@ session_start();
 
 if ($_SESSION["ingelogd"]=="ok")
 {
-    print "Welkom op deze homepage<br><br>";
+    // print "Welkom op deze homepage<br><br>";
     print "<form action='uitloggen.php' method='post'>";
-    print "<input type='submit' name='uitloggen' value='UITLOGGEN'>";
+    // print "<input type='submit' name='uitloggen' value='UITLOGGEN'>";
     print "</form>";
 }
 else
@@ -13,3 +13,24 @@ else
     header("location:login.php?message=Je bent niet ingelogd!");
 }   
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account</title>
+    <link rel="stylesheet" media="screen and (min-width:1500px)" href="full screen.css">
+    <link rel="stylesheet" media="screen and (max-width:600px)" href="phone.css">
+    <link rel="stylesheet" media="screen and (min-width:600px) and (max-width:1500px)" href="Tablet.css">
+</head>
+<body>
+<div class="logo">
+        <a href="homepagina.html"><img src="afbeeldingen/Knipsel_garage_50.png" alt="logo"></a>
+    </div>
+
+    <h1>Garage Degroote</h1>
+    <hr>
+    hello
+</body>
+</html>
