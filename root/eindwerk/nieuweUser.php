0318e527439abf256ba27ed324d1b0af2de32049
leavemygame <115145061+leavemygame@users.noreply.github.com>
2023-04-27 17:25:36 +0200

    qsdf
    qsdf

--- root/eindwerk/nieuweUser.php
+++ root/eindwerk/nieuweUser.php
@@ -9,29 +9,33 @@ else
 {
     include_once("connect.php"); //once want je zal hier maar één keer de connectie maken
 
-    $username = $_POST['gebruiker'];
-    $password = $_POST['paswoord'];
+    //$username = $_POST['gebruiker'];
+    //$password = $_POST['paswoord'];
+    $user = $_POST['gebruiker'];
+    $pasw = $_POST['paswoord'];
     $passwordcheck = $_POST['checkpaswoord'];
+    print "user: ".$user."<br>";
+    print "pasw: ".$pasw;
 
-    if ($password == $passwordcheck)
+    if ($pasw == $passwordcheck)
     {
     //controleer of de gebruikersnaam al bestaat
-        $query = "SELECT Username FROM inloggegevens WHERE Username = '$username'";
+        $query = "SELECT * FROM inloggegevens WHERE Username = '".$username."'";
         $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
         $count = mysqli_num_rows($result);
 
         if ($count == 0)//zorg dat een gebruiker uniek is, bestaat er al een gelijkaardig record?
         {
-            $query = "INSERT INTO inloggegevens (Username,Password,Type) VALUES ($username,$password,2)";
+            $query = "INSERT INTO inloggegevens (Username,Password,Type) VALUES ('".$user."','".$pasw."','2')";
             $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
 
 
-            $query = "SELECT Username, 'Password' FROM inloggegevens WHERE Username='$username' and 'Password' = '$password'";
+            $query = "SELECT * FROM inloggegevens WHERE Username='".$username."' and Password = '".$password."'";
             $result = mysqli_query($conn, $query) or die (mysqli_error($conn));
             $count = mysqli_num_rows($result);
             
 
-            if ($count >= 1) // controleer of de insert into gelukt is (dubbele controle)
+            if ($count <= 1) // controleer of de insert into gelukt is (dubbele controle)
             {
                 header('Location: registreer.php?message="Jouw account is toegevoegd."');
             }
@@ -49,6 +53,7 @@ else
     else
     {
         // wachtwoorden kloppen niet
+        print "Wachtwoord is fout";
     }
 
    
